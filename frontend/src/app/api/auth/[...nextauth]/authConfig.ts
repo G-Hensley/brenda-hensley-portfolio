1ed2ac46f0deeab0e0a787c9ab8f246db0d1b396
Gavin Hensley <gavinhensley@protonmail.com>
2025-05-04 15:11:56 -0400

    Set up shadcn

---
+++ frontend/src/app/api/auth/[...nextauth]/authConfig.ts
@@ -0,0 +1,26 @@
+import GithubProvider from 'next-auth/providers/github';
+import type { AuthOptions } from 'next-auth';
+import type { User } from 'next-auth';
+import type { Session } from 'next-auth';
+
+export const authConfig: AuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }: { user: User }) {
+      const allowedEmails = [
+        'gavinhensley@protonmail.com',
+        'hensley.brenda@protonmail.com',
+        'renaeehensley@gmail.com',
+      ];
+      return user.email ? allowedEmails.includes(user.email) : false;
+    },
+    async session({ session }: { session: Session }) {
+      return session;
+    },
+  },
+};
--- frontend/src/app/auth/page.tsx
+++ frontend/src/app/auth/page.tsx
@@ -1,14 +1,35 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import dotenv from "dotenv";
+'use client';
 
-dotenv.config();
+import { signIn, getProviders } from 'next-auth/react';
+import type { ClientSafeProvider } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 
-export default NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
-    })
-  ]
-})
+export default function SignIn() {
+  const [providers, setProviders] = useState<Record<
+    string,
+    ClientSafeProvider
+  > | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const providers = await getProviders();
+      setProviders(providers);
+    };
+    fetchProviders();
+  }, []);
+
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-4xl font-bold'>Sign In</h1>
+      {providers &&
+        Object.values(providers).map((provider: ClientSafeProvider) => (
+          <button
+            key={provider.name}
+            onClick={() => signIn(provider.id)}
+            className='mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 hover:scale-105 transition-all duration-300 cursor-pointer'>
+            Sign in with {provider.name}
+          </button>
+        ))}
+    </div>
+  );
+}
