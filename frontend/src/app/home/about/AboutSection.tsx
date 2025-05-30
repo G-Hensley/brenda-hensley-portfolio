310ed9cb2b4b5c4f09a94710e0f4878068995efd
Gavin Hensley <gavinhensley@protonmail.com>
2025-05-30 05:31:45 -0400

    Add icons to about section

--- frontend/src/app/home/about/AboutSection.tsx
+++ frontend/src/app/home/about/AboutSection.tsx
@@ -4,6 +4,7 @@ import WebGLBackground from "@/components/WebGlBackground";
 import { useRef, useState, useEffect } from "react";
 import { Electrolize } from "next/font/google";
 import { useInView } from "framer-motion";
+import { User } from "lucide-react";
 
 const electrolize = Electrolize({
   weight: ['400'],
@@ -26,11 +27,12 @@ export default function AboutSection() {
 
   return (
     <section id="about" className="relative px-6 pt-20 pb-16 bg-transparent flex flex-col gap-12 items-center">
-        <div ref={typingRef} className={`text-center text-3xl md:text-6xl text-green-600 text-glow ${electrolize.className} z-10`}>
+        <div ref={typingRef} className={`text-center flex items-center text-3xl md:text-6xl text-green-600 text-glow ${electrolize.className} z-10`}>
+          <User className="text-green-600 icon-glow mr-4" size={56} />
             {startTyping && (
               <>
                 <TypeAnimation
-                  sequence={[800, '# About Me', 1000]}
+                  sequence={[800, 'echo $USER', 1000]}
                   speed={50}
                   cursor={false}
                   repeat={0}
